2d2585299509e76feaa39957b9039e78560d2f59
tsunlamchan <tsunlamc@nycstudents.net>
2026-01-06 19:05:02 +0000

    3_3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,16 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function flower(){
+    document.getElementById("highlight").innerHTML="flower";
+}
+function flower2(){
+    document.getElementById("highlight").innerHTML="flower2";
+}
+function flower3(){
+    document.getElementById("highlight").innerHTML='flower3';
+}
+function flower4(){
+    document.getElementById("highlight").innerHTML='flower4';
+}
+function flower5(){
+    document.getElementById("highlight").innerHTML='flower5';
+}
